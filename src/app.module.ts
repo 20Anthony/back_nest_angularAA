f1eab54c3c65274261cb3f91b72123523e1c2833
Anthony Alarcón <ara.alarcon@yavirac.edu.ec>
2025-10-08 11:10:14 -0500

    clase 28

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { ModulesModule } from './modules/modules.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ModulesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/auth/auth-jwt-pasport.md
@@ -0,0 +1,20 @@
+# Instalacion
+```
+npm i --save@nest.js/jwt passport-jwt bcrypt
+npm i --save-dev @types/passport-jwt
+```
+
+# Arrancamos el proyecto
+```
+npm run start:dev
+```
+# modules & resource
+```
+nest g mo modules/auth
+nest g res modelues/users
+```
+# controllers & services
+```
+nest g co modelues/auth
+nest g s modeles/auth
+```
