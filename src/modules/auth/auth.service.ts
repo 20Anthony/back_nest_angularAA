0fffd612e9f9cd51b9ecd65375f3a2dcc2b18b09
Anthony Alarcón <15anthonydj@gmail.com>
2025-10-15 13:36:24 -0500

    39

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { LoginAuthDto } from './dto/login-auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+    
+    login(credenciales: LoginAuthDto){
+
+    }
+}
--- src/modules/auth/dto/register-auth.dto.ts
+++ src/modules/auth/dto/register-auth.dto.ts
@@ -1,9 +1,17 @@
 import {PartialType} from "@nestjs/mapped-types"
 import { LoginAuthDto } from "./login-auth.dto"
-import {IsNotEmpty, } from "class-validator"
+import {IsEmail, IsNotEmpty, MaxLength, MinLength, } from "class-validator"
 
 export class RegisterAuthDto extends PartialType(LoginAuthDto){
+    id?:number;
     @IsNotEmpty()
     name: string;
+    @IsNotEmpty()
+    @IsEmail()
+    email: string;
+    @IsNotEmpty()
+    @MaxLength(20)
+    @MinLength(6)
+    password: string;
 
 }
