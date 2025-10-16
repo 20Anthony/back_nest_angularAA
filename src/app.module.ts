28d28a386d6a9d7aca6bacf475eb0a809392a0ef
Anthony Alarcón <ara.alarcon@yavirac.edu.ec>
2025-10-16 13:49:03 -0500

    22

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { ModulesModule } from './modules/modules.module';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ModulesModule],
+  imports: [AuthModule, UserModule, ModulesModule, DatabaseModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
